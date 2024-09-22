e2f1241467884009ef618b8dfb04a98816f76592
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-09-22 14:43:25 -0600

    Se añadio clase portada para mostar la portada animada Se añadio el sonido moneda.wav

---
+++ La maquina enigma/Portada.h
@@ -0,0 +1,26 @@
+#ifndef PORTADA_H
+#define PORTADA_H
+
+#include <string>
+
+using namespace std;
+
+class Portada {
+private:
+	string materia, grupo, profesor;
+public:
+	//Constructor
+	Portada();
+	//Destructor
+	~Portada();
+	void mostrarPortada();
+	void setVariables(string, string, string);//método setter
+protected:
+	void irA(int, int);
+	void ocultarCursor();
+	void mostrarCursor();
+	void portada();
+	void animacion();
+};
+
+#endif
